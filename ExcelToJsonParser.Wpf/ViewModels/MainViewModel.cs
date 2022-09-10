57c32f254186794195875189a04c954dab3de94e
Daria Semenova <dasha.ign,semenova@gmail.com>
2022-09-10 13:19:37 +0300

    Add project files.

---
+++ ExcelToJsonParser.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToJsonParser.Wpf.ViewModels
+{
+    internal class MainViewModel : ViewModel
+    {
+        #region view model properties
+
+        
+        #endregion
+
+        #region view model commands
+        #endregion
+    }
+}
